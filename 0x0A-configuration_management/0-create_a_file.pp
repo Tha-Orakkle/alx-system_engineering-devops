e1a6b64f1e843245e6a0ee581117f24af7b4ea2f
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-06-24 05:16:42 +0100

    configuration managemenr basics

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# creates a file in /tmp
+
+file { 'school':
+   path    => '/tmp/school',
+   content => 'I love Puppet',
+   group   => 'www-data',
+   owner   => 'www-data',
+   mode    => '0744'
+}
---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,5 @@
+# kills a process named 'killmenow'
+
+exec { 'pkill':
+   path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
