c9f7b79d494d42b1c699ed7719d2c3f35f560c8d
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-07-04 07:18:06 -0700

    webserver configuration using puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,38 @@
+# Installs and configures Nginx web server with Puppet
+
+new_page="https://www.youtube.com/watch?v=EJC-_j3SnXk"
+red_line="\trewrite ^/redirect_me/$ ${new_page} permanent;"
+
+exec { 'update packages':
+	provider => shell,
+	command  => 'sudo apt-get update'
+}
+
+package { 'nginx':
+	ensure  => 'installed',
+	require => Exec['update packages']
+}
+
+exec { 'restart nginx':
+	provider => shell,
+	command  => 'sudo service nginx restart',
+	require  => Package['nginx']
+}
+
+file { '/var/www/html/index.html':
+	ensure  => 'present',
+	content => 'Hello World!',
+	mode    => '0644',
+	owner   => 'root',
+	group   => 'root'
+}
+
+file_line { 'set 301 redirection':
+	ensure   => 'present',
+	after    => 'server_name _;',
+	path     => '/etc/nginx/sites-available/default',
+	multiple => 'false',
+	line     => $red_line,
+	notify   => Exec['restart nginx'],
+	require  => File['/var/www/html/index.html']
+}
