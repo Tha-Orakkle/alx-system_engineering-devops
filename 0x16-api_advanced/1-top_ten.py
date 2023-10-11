fd8291d775fe93c7d5315d68aa9f55016477f0b0
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-10-11 09:37:42 +0100

    top ten hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -18,7 +18,8 @@ def top_ten(subreddit):
             }
     params = {'limit': 10}
 
-    response = requests.get(url, headers=headers, params=params)
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
     if response.status_code == 200:
         posts = response.json().get("data").get("children")
